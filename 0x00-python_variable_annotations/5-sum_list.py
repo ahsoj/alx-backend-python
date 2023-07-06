9b35a5f2f5ee1e27462bcc86ecad36549cce039f
eyasu <daa282386@gmail.com>
2023-07-06 13:22:09 +0300

    variable_annotations

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    typed function
+"""
+# from typing import List
+
+
+def sum_list(input_list: list[float]) -> float:
+    """return sum of a list[floats]"""
+    return float(sum(input_list))
