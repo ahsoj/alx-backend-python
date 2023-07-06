27b785bf4334a031318bff6effd0ad0f7dccba9b
eyasu <daa282386@gmail.com>
2023-07-06 12:54:43 +0300

    variable_annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    typed concat function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concatination of two strings
+    rType: str
+    """
+    return concat(str1, str2)
