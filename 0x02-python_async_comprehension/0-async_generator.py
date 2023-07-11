855be5d3f868422db0549b0652cd5ef9994e4d00
eyasu <daa282386@gmail.com>
2023-07-11 15:19:34 +0300

    python_async_comphrehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,5 +7,5 @@ import random
 async def async_generator():
     """Async generator function."""
     for _ in range(10):
-        await asyncio.sleep(1)
         yield random.random() * 10
+        await asyncio.sleep(1)
