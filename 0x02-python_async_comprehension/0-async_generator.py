d143f48e4be0bd494ec89d4d292f81d245f02791
eyasu <daa282386@gmail.com>
2023-07-11 15:14:56 +0300

    python_async_comphrehension

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""async genrator"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """Async generator function."""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+"""async comprehension"""
+import asyncio
